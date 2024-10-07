547f3e764aafcc572e74d2657ea626b13be174ae
Sabeeh <61093501+SabeehHassany@users.noreply.github.com>
2024-10-06 22:12:29 -0400

    json update

--- redis.go
+++ redis.go
@@ -3,45 +3,6 @@ package main
 import (
 	"context"
 	"log"
-	"os"
-
-	"github.com/go-redis/redis/v8"
-)
-
-var redisClient *redis.Client
-var ctx = context.Background()
-
-// Define the Redis channel for new jots notifications
-const newJotsChannel = "new_jots_channel"
-
-func init() {
-	// Get the Redis URL from the environment variable
-	redisURL := os.Getenv("REDIS_URL")
-	if redisURL == "" {
-		log.Fatalf("REDIS_URL not set")
-	}
-
-	opt, err := redis.ParseURL(redisURL)
-	if err != nil {
-		log.Fatalf("Failed to parse Redis URL: %v", err)
-	}
-
-	redisClient = redis.NewClient(opt)
-
-	_, err = redisClient.Ping(ctx).Result()
-	if err != nil {
-		log.Fatalf("Failed to connect to Redis: %v", err)
-	}
-
-	log.Println("Connected to Redis successfully")
-}
-
-/*
-package main
-
-import (
-	"context"
-	"log"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -66,4 +27,3 @@ func init() {
 
 	log.Println("Connected to Redis successfully")
 }
-*/
